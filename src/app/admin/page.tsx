6d483f689392c4cbcda86a82e9d51f716385fbfa
seung1 <97tmddnjs@naver.com>
2023-10-08 07:57:18 +0900

    navbar 추가, 페이지 추가

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminPage = () => {
+  return <div>AdminPage / 관리자만 접근 가능</div>;
+};
+
+export default AdminPage;
---
+++ src/app/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserPage = () => {
+  return <div>UserPage / 로그인된 유저만 접근가능</div>;
+};
+
+export default UserPage;
